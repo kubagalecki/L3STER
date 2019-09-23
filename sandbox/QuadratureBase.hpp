a2d81da1000ec0c783969debe90b7920bf87d4c1
Jakub Gałecki <j.galecki@quickersim.com>
2019-09-23 11:14:10 +0200

    cleanup

---
+++ sandbox/QuadratureBase.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ElementTypes.hpp"
+
+class QuadratureBase
+{
+
+};
---
+++ sandbox/ReferenceElementBase.hpp
@@ -0,0 +1,36 @@
+// ReferenceElementBase class implementation
+
+#pragma once
+
+#include "ElementTypes.hpp"
+
+namespace lstr
+{
+	namespace mesh
+	{
+		// Class ReferenceElementBase needs to be specialized for each element type.
+		// The specializations must have the following methods:
+		//
+		// static constexpr size_t getNumberOfNodes(types::el_o_t)
+		// static constexpr types::el_dim_t getDim()
+
+		template <ElementTypes ELTYPE, typename CRTP_Child>
+		class ReferenceElementBase;
+
+		// QUAD
+		template <typename CRTP_Child>
+		class ReferenceElementBase<ElementTypes::Quad, CRTP_Child>
+		{
+		protected:
+			static constexpr size_t getNumberOfNodes(types::el_o_t elorder)
+			{
+				return (elorder + 1) * (elorder + 1);
+			}
+
+			static constexpr types::el_dim_t getDim()
+			{
+				return 2;
+			}
+		};
+	}
+}
