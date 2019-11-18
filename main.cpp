a2c22ede60f5495dbf0c8f171a33f86f78e9dd18
Jakub Gałecki <j.galecki11@gmail.com>
2019-11-18 20:01:21 +0100

    lagrangeFit argument deduction not working, fix needed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include "Domain.hpp"
+#include "MeshMaster.hpp"
+#include "Polynomial.hpp"
 #include <iostream>
 #include <exception>
 
@@ -12,6 +13,11 @@ int main()
         d.emplaceBack<lstr::mesh::ElementTypes::Quad, 1>(NA);
 
         auto&& Q = lstr::quad::Quadrature<lstr::mesh::ElementTypes::Quad, lstr::quad::QuadratureTypes::GLeg, 2>{};
+        
+        using a_t = std::array<lstr::types::val_t, 2>;
+        const auto x = std::array<lstr::types::val_t, 2>{0., 1.};
+        const auto y = std::array<lstr::types::val_t, 2>{0., 1.};
+        auto p2 = lstr::util::lagrangeFit(x, y);
         
         return 0;
     }
--- src/utilities/Polynomial.hpp
+++ src/utilities/Polynomial.hpp
@@ -8,8 +8,16 @@
 #define L3STER_INCGUARD_UTIL_POLYNOMIAL_HPP
 
 #include "Types.h"
+#include "Eigen/Dense"
 
 #include <array>
+#include <algorithm>
+#include <type_traits>
+#include <initializer_list>
+#include <exception>
+#include <string>
+#include <iterator>
+#include <numeric>
 
 namespace lstr
 {
@@ -25,47 +33,229 @@ namespace lstr
         class Polynomial final
         {
         public:
+            template <types::poly_o_t I>
+            friend class Polynomial;
+            
+            template<types::poly_o_t I>
+            friend Polynomial<I> lagrangeFit(std::array<types::val_t, I + 1>, std::array<types::val_t, I + 1>);
+
+            //Aliases
+            using array_t = std::array < types::val_t, N + 1 >;
+
             // Ctors & Dtors
-            // Forwarding constructor for the underlying array
+            Polynomial(std::initializer_list<types::val_t>);
+
             template <typename ... Types>
             Polynomial(const Types& ... args) : coefs(args ...) {}
 
             // Access
-            
-			// Operations
-			types::val_t eval(const types::val_t&);				// evaluate polynomial
+            types::val_t* data() { return coefs.data(); }       // access the underlying array
+
+            // Polynomial manipulations
+            // evaluate polynomial
+            types::val_t                                eval(const types::val_t&);
+            Polynomial < N - 1 >                        polyder();
+            Polynomial < N + 1 >                        polyint();
+            std::array<std::complex<types::val_t>, N>   roots();
+
+            // Operators
+            Polynomial<N> operator+=(types::val_t _a) { coefs.front() += _a; return *this; }    // increment by cosntant
+            Polynomial<N> operator*=(types::val_t);
+
         private:
-            std::array <types::val_t, N + 1> coefs;
+            array_t coefs;
+        };
+
+        // Forward declare specialization of template
+        template<>
+        class Polynomial<0>;
+
+        // Initializer list constructor
+        template <types::poly_o_t N>
+        Polynomial<N>::Polynomial(std::initializer_list<types::val_t> init_list)
+        {
+            if (init_list.size() != N + 1)
+            {
+                throw (std::length_error("Incorrect number of coefficients for polynomial of specified order. Should be: "
+                                         + std::to_string(N + 1) + ", is: " + std::to_string(init_list.size()) + '\n'));
+            }
+
+            std::move(init_list.begin(), init_list.end(), coefs.begin());
         }
 
-		template <types::poly_o_t N>
-		types::val_t Polynomial::eval(const types::val_t& x)
-		{
-			// evaluate p(x)
-			types::val_t ret_val = coefs.front();
-			auto current_exp = x;
-			for (int i = 1; i < N i++)
-			{
-				ret_val += current_exp * coefs[i];
-				current_exp *= x;
-			}
-			ret_val += current_exp * coefs.back();
-		}
+        // Polynomial evaluation
+        template <types::poly_o_t N>
+        types::val_t Polynomial<N>::eval(const types::val_t& x)
+        {
+            // evaluate p(x)
+            auto                ret_val     = coefs.front();
+            auto                current_exp = x;
 
-		// Specialization for order 0 polynomial
-		template<>
-		class Polynomial<0>
-		{
-		public:
-			// Ctors & Dtors
-			Polynomial()							: a0(0)		{}
-			Polynomial(const types::val_t& _a0)		: a0(_a0)	{}
+            auto op = [&](types::val_t c) -> void
+            {
+                ret_val         += current_exp * c;
+                current_exp     *= x;
+            };
 
-			// Operations
-			types::val_t eval(const types::val_t&)				{ return a0; }
-		private:
-			types::val_t a0;
-		};
+            std::for_each(++coefs.begin(), --coefs.end(), op);
+
+            ret_val += current_exp * coefs.back();
+            return ret_val;
+        }
+
+        // Polynomial dervative
+        template <types::poly_o_t N>
+        Polynomial < N - 1 > Polynomial<N>::polyder()
+        {
+            // Handle conversion to proxy type Polynomial<0>
+            if (N == 1)       return Polynomial<0> {coefs[1]};
+
+            auto ret_val      = Polynomial < N - 1 > {};
+
+            auto current_exp  = N;
+
+            auto op = [&](const typename array_t::value_type & a_old)
+            {
+                return a_old * (current_exp--);
+            };
+
+            std::transform(coefs.crbegin(), std::prev(coefs.crend()), ret_val.coefs.rbegin(), op);
+
+            return ret_val;
+        }
+
+        // Polynomial integral, C = 0
+        template <types::poly_o_t N>
+        Polynomial < N + 1 > Polynomial<N>::polyint()
+        {
+            auto ret_val            = Polynomial < N + 1 > {};
+            ret_val.coefs.front()   = 0.;
+            auto current_exp        = 1;
+
+            auto op = [&](const typename array_t::value_type & a_old)
+            {
+                return a_old / (current_exp++);
+            };
+
+            std::transform(coefs.begin(), coefs.end(), std::next(ret_val.coefs.begin()), op);
+            return ret_val;
+        }
+
+        // Polynomial roots (complex valued)
+        template <types::poly_o_t N>
+        std::array<std::complex<types::val_t>, N> Polynomial<N>::roots()
+        {
+            using complex_t = std::complex<types::val_t>;
+
+            // If linear function, x0 = -b/a
+            if (N == 1)
+                return std::array<complex_t, N> { complex_t{coefs.front() / coefs.back(), 0.} };
+
+            // Create and populate companion matrix
+            auto comp_mat = Eigen::Matrix<types::val_t, N, N> {};
+
+            for (auto i = 0; i < N; i++)
+            {
+                for (auto j = 0; j < N; j++)
+                {
+                    if (i == j + 1)
+                    {
+                        comp_mat(i, j) = 1.;
+                    }
+                    else
+                    {
+                        if (j == N - 1)
+                        {
+                            comp_mat(i, j) = -coefs[i] / coefs.back();
+                        }
+                        else
+                        {
+                            comp_mat(i, j) = 0;
+                        }
+                    }
+                }
+            }
+
+            // Get Eigen::Vector of eigenvalues
+            auto eig = comp_mat.eigenvalues();
+
+            // Copy to the returned array
+            auto ret_val = std::array<complex_t, N> {};
+            std::copy(eig.begin(), eig.end(), ret_val.begin());
+
+            // Sort by absolute value
+            std::sort(ret_val.begin(), ret_val.end(),
+            [](complex_t a, complex_t b) { return std::abs(a) < std::abs(b); });
+
+            return ret_val;
+        }
+
+        // Scaling
+        template <types::poly_o_t N>
+        Polynomial<N> Polynomial<N>::operator*=(types::val_t _a)
+        {
+            auto op = [&](types::val_t c) { c *= _a; };
+            std::for_each(coefs.begin(), coefs.end(), op);
+            return *this;
+        }
+
+        // Specialization for order 0 polynomial
+        template<>
+        class Polynomial<0>
+        {
+        public:
+            // Ctors & Dtors
+            Polynomial()                            : a0(0)     {}
+            Polynomial(const types::val_t& _a0)     : a0(_a0)   {}
+
+            // Access
+            types::val_t* data() { return &a0; }       // access the underlying coefficient
+
+            // Polynomial manipulations
+            types::val_t    eval(const types::val_t&)   { return a0; }
+            Polynomial<0>   polyder()                   { return Polynomial<0> {0.}; }
+            Polynomial<1>   polyint()                   { return Polynomial<1> {0., a0}; }
+
+            // Operators
+            Polynomial<0>   operator+= (types::val_t _a) { a0 += _a; return *this; }
+            Polynomial<0>   operator*= (types::val_t _a) { a0 *= _a; return *this; }
+
+        private:
+            types::val_t a0;
+
+        public:
+            // Although not used, empty coefs array is required for correct conversion
+            // This is a bit of a dirty hack
+            std::array<types::val_t, 0> coefs;
+        };
+
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        //                                          FUNTIONS                                        //
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Lagrange interpolation
+        /*
+        Returns the polynomial p of order N, such that for all 0 <= i < N p(x[i]) = y[i]
+        */
+        template <types::poly_o_t N>
+        Polynomial<N> lagrangeFit(const std::array<types::val_t, N + 1>& x, const std::array<types::val_t, N + 1>& y)
+        {
+            auto A = Eigen::Matrix < types::val_t, N + 1, N + 1 > {};
+            auto b = Eigen::Matrix < types::val_t, N + 1, 1 > {};
+            
+            for (auto i = 0; i <= N; i++)
+            {
+                b[i] = y[i];
+                A(i, 0) = 1.;
+                for (auto j = 1; j <= N; j++)
+                    A(i, j) = A(i, j - 1)*x[i];
+            }
+            
+            auto ret_coefs = A.colPivHouseholderQr().solve(b);
+            
+            auto ret_val = Polynomial<N>{};
+            std::copy(ret_coefs.cbegin(), ret_coefs.cend(), ret_val.coefs.begin());
+            return ret_val;
+        }
     }
 }
 
