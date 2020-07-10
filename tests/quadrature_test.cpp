f55a39d1a35d933267841ee7bc0a39db6751bbc3
Jakub Galecki <j.galecki@quickersim.com>
2020-07-10 18:52:08 +0200

    setting up CI using github actions

---
+++ tests/quadrature_test.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+#include "l3ster.h"
+
+TEST_CASE("Quadratures are computed correctly", "[quadrature]")
+{
+    lstr::mesh::Element< lstr::mesh::ElementTypes::Quad, 1 >                element{{1, 2, 3, 4}};
+    lstr::quad::QuadratureGenerator< lstr::quad::QuadratureTypes::GLeg, 1 > quad_gen;
+    const auto& quad  = quad_gen.get(element);
+    const auto& q_pts = quad.getQPoints();
+    const auto& q_w   = quad.getWeights();
+
+    REQUIRE(q_pts.size() == q_w.size());
+}
