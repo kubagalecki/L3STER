bdfc05eef36276dd3320aa5eb7da8e0c7b4a7a56
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2020-09-15 14:00:30 +0200

    Added missing Dockerfile and cmake build flag

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+COPY ./ ./
+
+RUN chmod +x scripts/ci/entrypoint.sh
+
+ENTRYPOINT ["sh", "scripts/ci/entrypoint.sh"]
--- scripts/ci/entrypoint.sh
+++ scripts/ci/entrypoint.sh
@@ -3,12 +3,12 @@
 echo "Starting CI build..."
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DL3STER_ENABLE_TESTS=ON ..
 cmake --build .
 echo "Running tests..."
 ctest && TEST_STATUS=true || TEST_STATUS=false
-$TEST_STATUS || echo "Tests failed. Delaying error until after code coverage is reported"
+$TEST_STATUS || echo "Tests failed. Delaying error until after code coverage is reported..."
 echo "Generating code coverage report and uploading to Codecov..."
 gcovr -x -r .. -e ../tests -o report.xml
 curl -s https://codecov.io/bash | bash -s -- -c -f report.xml -t $INPUT_CODECOV_TOKEN
-$TEST_STATUS && echo "All tests passed. Good job! Finishing..." && exit 0 || echo "Some tests failed. I'm going to crash now. Goodbye." && exit 1
+$TEST_STATUS && echo "All tests passed. Finishing..." && exit 0 || echo "Some tests failed. Error wil now be reported..." && exit 1
