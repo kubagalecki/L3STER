55afc90320c4c0a1f67b0033700365db8837c9a7
Jakub Gałecki <jgalecki11@gmail.com>
2020-11-16 10:33:39 +0100

    Renamed CMake scripts, small CI script refactor

--- scripts/ci/entrypoint.sh
+++ scripts/ci/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh -l
 
-echo "Starting CI build..."
+echo "Loading Trilinos from spack..."
 . /spack/share/spack/setup-env.sh
 spack load trilinos
+
+echo "Building L3STER tests..."
 mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DL3STER_ENABLE_TESTS=ON ..
-cmake --build .
-echo "Running tests..."
+cd build || exit
+cmake -DCMAKE_BUILD_TYPE=Debug -DL3STER_ENABLE_TESTS=ON .. || exit
+cmake --build . || exit
+
+echo "Running L3STER tests..."
 ctest && TEST_STATUS=true || TEST_STATUS=false
 $TEST_STATUS || echo "Tests failed. Delaying error until after code coverage is reported..."
+
 echo "Generating code coverage report and uploading to Codecov..."
 gcovr -x -r .. -e ../tests -o report.xml
-curl -s https://codecov.io/bash | bash -s -- -c -f report.xml -t $INPUT_CODECOV_TOKEN
-$TEST_STATUS && echo "All tests passed. Finishing..." && exit 0 || echo "Some tests failed. Error wil now be reported..." && exit 1
+curl -s https://codecov.io/bash | bash -s -- -c -f report.xml -t "$INPUT_CODECOV_TOKEN"
+$TEST_STATUS && echo "All tests passed. Finishing..." && exit || \
+  echo "Some tests failed. Error wil now be reported..." && exit 1
