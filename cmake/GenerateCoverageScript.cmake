7df67f353d224d733dadcd558d36c536900dce4d
kubagalecki <j.galecki11@gmail.com>
2021-03-16 15:46:36 +0100

    added gmon prefix and better gcovr options

--- cmake/GenerateCoverageScript.cmake
+++ cmake/GenerateCoverageScript.cmake
@@ -1,20 +1,15 @@
 function( generate_coverage_script )
     set( script_file ${CMAKE_CURRENT_BINARY_DIR}/generate_coverage_report.sh )
     file( WRITE ${script_file}
-          "gcovr --json -r ${L3STER_DIR} -e ${L3STER_DIR}/tests -o unit_tests_report.json\n"
+          "gcovr -x -r ${L3STER_DIR} -e ${L3STER_DIR}/tests -o coverage_report.xml\n"
           )
     foreach ( test ${L3STER_MPI_TESTS} )
         file( APPEND ${script_file}
               "cd ${test}\n"
-              "gcovr --json -r ${L3STER_DIR} -e ${L3STER_DIR}/tests -o ${test}_report.json\n"
+              "gcovr -x -r ${L3STER_DIR} -e ${L3STER_DIR}/tests -o coverage_report.xml\n"
               "cd ..\n"
               )
     endforeach ()
-    file( APPEND ${script_file} "gcovr -a ${CMAKE_CURRENT_BINARY_DIR}/unit_tests_report.json \\\n" )
-    foreach ( test ${L3STER_MPI_TESTS} )
-        file( APPEND ${script_file} "-a ${CMAKE_CURRENT_BINARY_DIR}/${test}/${test}_report.json \\\n" )
-    endforeach ()
-    file( APPEND ${script_file} "--json -r ${L3STER_DIR} -o coverage_report.json" )
 endfunction()
 
 generate_coverage_script()
--- scripts/ci/entrypoint.sh
+++ scripts/ci/entrypoint.sh
@@ -7,11 +7,14 @@ cmake \
   -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_PATH \
   -DL3STER_ENABLE_TESTS=ON \
   .. || exit 1
+if [ "$REPORT_COVERAGE" != "" ]; then
+  export GMON_OUT_PREFIX=profile_data
+fi
 cmake --build . -- -j || exit 1
 ctest --output-on-failure --repeat until-pass:2 --timeout 120 || exit 1
 if [ "$REPORT_COVERAGE" != "" ]; then
-  # chmod +x generate_coverage_report.sh
-  # ./generate_coverage_report.sh || exit 1
+  chmod +x generate_coverage_report.sh
+  ./generate_coverage_report.sh || exit 1
   curl -s https://codecov.io/bash >codecov.sh || exit 1
   chmod +x codecov.sh
   ./codecov.sh -Z || exit 1
