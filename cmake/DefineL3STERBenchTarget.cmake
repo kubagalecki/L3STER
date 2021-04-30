560c9be21cad0bc1a4ea245a27a0e82a2d5c5834
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-04-30 10:23:30 +0200

    Benchmarks - added benchmark target to cmake - benchmarks not included in CI - added benchmark for boundary view generation - cosmetic changes in some unrelated cmake files

---
+++ cmake/DefineL3STERBenchTarget.cmake
@@ -0,0 +1,11 @@
+add_executable( L3STER_benchmarks
+                ${L3STER_DIR}/benchmarks/BenchmarksMain.cpp
+                ${L3STER_DIR}/benchmarks/BoundaryView.cpp
+                )
+
+set( L3STER_BENCH_DATA_PATH "${L3STER_DIR}/benchmarks/data" CACHE STRING "Path to benchmark data directory" )
+configure_file( ${L3STER_DIR}/benchmarks/DataPath.h.in DataPath.h )
+target_include_directories( L3STER_benchmarks PRIVATE ${CMAKE_CURRENT_BINARY_DIR} )
+
+find_package( benchmark REQUIRED )
+target_link_libraries( L3STER_benchmarks PRIVATE L3STER benchmark::benchmark )
--- cmake/DefineL3STERTestTarget.cmake
+++ cmake/DefineL3STERTestTarget.cmake
@@ -1,4 +1,3 @@
-# Register L3STER tests with CTest
 add_executable( L3STER_tests
                 ${L3STER_DIR}/tests/TestsMain.cpp
                 ${L3STER_DIR}/tests/MathTests.cpp
@@ -8,10 +7,9 @@ add_executable( L3STER_tests
                 ${L3STER_DIR}/tests/NodeAllocationTests.cpp )
 
 set( L3STER_TESTS_DATA_PATH "${L3STER_DIR}/tests/data" CACHE STRING "Path to test data directory" )
-set( L3STER_N_NUMA_NODES 1 CACHE STRING "Number of NUMA nodes of current machine" )
+set( L3STER_N_NUMA_NODES 1 CACHE STRING "Number of NUMA nodes of current machine" ) # TODO: automate
 configure_file( ${L3STER_DIR}/tests/TestDataPath.h.in TestDataPath.h )
 target_include_directories( L3STER_tests PRIVATE ${CMAKE_CURRENT_BINARY_DIR} )
-
 target_link_libraries( L3STER_tests
                        L3STER
                        Catch2::Catch2
@@ -20,5 +18,4 @@ target_link_libraries( L3STER_tests
 catch_discover_tests( L3STER_tests )
 
 include( ${L3STER_DIR}/cmake/RegisterMpiTests.cmake )
-
 include( ${L3STER_DIR}/cmake/GenerateCoverageScript.cmake )
