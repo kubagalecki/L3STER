9a994fc8e9449f346d046003cf42a114cdf8ba4d
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-05-18 16:12:38 +0200

    removed unused space_dims array

--- include/mesh/Constants.hpp
+++ include/mesh/Constants.hpp
@@ -7,11 +7,7 @@
 
 namespace lstr
 {
-// Define allowed spatial dimensions
-constexpr inline std::array< dim_t, 3 > space_dims = {1, 2, 3};
-
 // Define allowed element orders
 constexpr inline std::array< el_o_t, 2 > element_orders = {1, 2};
 } // namespace lstr
-
 #endif // L3STER_MESH_CONSTANTS_HPP
