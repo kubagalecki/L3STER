aee40b51303b343aa6f0f2253af3a4bbf5880ec7
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-06-02 17:32:45 +0200

    Started work on simulation structure [skip ci]

--- include/util/Common.hpp
+++ include/util/Common.hpp
@@ -1,5 +1,8 @@
 #ifndef L3STER_UTIL_COMMON_HPP
 #define L3STER_UTIL_COMMON_HPP
+
+#include <type_traits>
+
 namespace lstr
 {
 template < typename... T >
@@ -9,5 +12,9 @@ struct OverloadSet : public T...
 };
 template < typename... T >
 OverloadSet(T&&...) -> OverloadSet< T... >;
+
+template < auto V >
+requires(std::is_enum_v< decltype(V) >) struct EnumTag
+{};
 } // namespace lstr
 #endif // L3STER_UTIL_COMMON_HPP
