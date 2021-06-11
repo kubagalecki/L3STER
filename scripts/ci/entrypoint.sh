ce75fb50ea4f6117a335a78c94e6cb30fe853f41
Jakub Gałecki <33153107+kubagalecki@users.noreply.github.com>
2021-06-11 10:58:33 +0200

    Codecov update (#11)
    * codecov now uses the binary uploader with SHA check
    
    * added shasum to base container
    
    * arch-specific shasum command
    
    * corrected sha command
    
    * codecov ignores build directory

--- scripts/ci/entrypoint.sh
+++ scripts/ci/entrypoint.sh
@@ -15,7 +15,12 @@ ctest --output-on-failure --repeat until-pass:2 --timeout 600 || exit 1
 if [ "$REPORT_COVERAGE" != "" ]; then
   chmod +x generate_coverage_report.sh
   ./generate_coverage_report.sh || exit 1
-  curl -s https://codecov.io/bash >codecov.sh || exit 1
-  chmod +x codecov.sh
-  ./codecov.sh -Z -X gcov || exit 1
+  curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --import || exit 1
+  curl -Os https://uploader.codecov.io/latest/codecov-linux || exit 1
+  curl -Os https://uploader.codecov.io/latest/codecov-linux.SHA256SUM || exit 1
+  curl -Os https://uploader.codecov.io/latest/codecov-linux.SHA256SUM.sig || exit 1
+  gpg --verify codecov-linux.SHA256SUM.sig codecov-linux.SHA256SUM || exit 1
+  sha256sum -c codecov-linux.SHA256SUM || exit 1
+  chmod +x codecov-linux
+  ./codecov-linux -Z -X gcov || exit 1
 fi
