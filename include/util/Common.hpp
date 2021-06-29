dd1836b122a63283b101b822df3c003bb41332ac
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-06-29 10:18:06 +0200

    ConstexprVector implementation ready for testing

--- include/util/Common.hpp
+++ include/util/Common.hpp
@@ -16,5 +16,16 @@ OverloadSet(T&&...) -> OverloadSet< T... >;
 template < auto V >
 requires(std::is_enum_v< decltype(V) >) struct EnumTag
 {};
+
+template < typename T1, typename T2 >
+constexpr bool isSameObject(T1& o1, T2& o2)
+{
+    if constexpr (std::is_same_v< T1, T2 >)
+        return std::addressof(o1) == std::addressof(o2);
+    else
+        return false;
+}
+template < typename T1, typename T2 >
+constexpr bool isSameObject(T1&&, T2&&) = delete;
 } // namespace lstr
 #endif // L3STER_UTIL_COMMON_HPP
