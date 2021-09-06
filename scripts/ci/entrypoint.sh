7c8eb9ac2e02a14a5bbee4347c86de8565eccbcc
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-09-06 11:54:12 +0200

    CI update

--- scripts/ci/entrypoint.sh
+++ scripts/ci/entrypoint.sh
@@ -3,10 +3,10 @@
 . /spack/share/spack/setup-env.sh
 spack load eigen catch2 intel-tbb trilinos
 mkdir build
-cd build
+cd build || exit 1
 cmake \
-  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
-  -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_PATH \
+  -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
+  -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_PATH" \
   -DL3STER_ENABLE_TESTS=ON \
   -DL3STER_ENABLE_VERBOSITY=ON \
   .. || exit 1
@@ -16,11 +16,11 @@ if [ "$REPORT_COVERAGE" != "" ]; then
   chmod +x generate_coverage_report.sh
   ./generate_coverage_report.sh || exit 1
   curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --import || exit 1
-  curl -Os https://uploader.codecov.io/latest/codecov-linux || exit 1
-  curl -Os https://uploader.codecov.io/latest/codecov-linux.SHA256SUM || exit 1
-  curl -Os https://uploader.codecov.io/latest/codecov-linux.SHA256SUM.sig || exit 1
-  gpg --verify codecov-linux.SHA256SUM.sig codecov-linux.SHA256SUM || exit 1
-  sha256sum -c codecov-linux.SHA256SUM || exit 1
-  chmod +x codecov-linux
-  ./codecov-linux -Z -X gcov || exit 1
+  curl -Os https://uploader.codecov.io/latest/linux/codecov || exit 1
+  curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM || exit 1
+  curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig || exit 1
+  gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM || exit 1
+  sha256sum -c codecov.SHA256SUM || exit 1
+  chmod +x codecov
+  ./codecov -Z -X gcov || exit 1
 fi
