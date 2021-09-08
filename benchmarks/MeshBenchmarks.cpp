624ad48d2b644838df4c70aefffe9d14256085b0
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-09-08 11:20:32 +0200

    added element iteration benchmarks [skip ci]

--- benchmarks/MeshBenchmarks.cpp
+++ benchmarks/MeshBenchmarks.cpp
@@ -8,7 +8,7 @@ void BM_MeshRead(benchmark::State& state)
     for (auto _ : state)
         const auto temp = lstr::readMesh(L3STER_TESTDATA_ABSPATH(sphere.msh), lstr::gmsh_tag);
 }
-BENCHMARK(BM_MeshRead)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_MeshRead)->Unit(benchmark::kMillisecond)->Name("Read mesh");
 
 void BM_DualGraphGeneration(benchmark::State& state)
 {
@@ -21,7 +21,7 @@ void BM_DualGraphGeneration(benchmark::State& state)
         partition.deleteDualGraph();
     }
 }
-BENCHMARK(BM_DualGraphGeneration)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_DualGraphGeneration)->Unit(benchmark::kMillisecond)->Name("Generate dual graph");
 
 void BM_BoundaryViewGeneration(benchmark::State& state)
 {
@@ -35,7 +35,7 @@ void BM_BoundaryViewGeneration(benchmark::State& state)
         benchmark::DoNotOptimize(boundary_view);
     }
 }
-BENCHMARK(BM_BoundaryViewGeneration)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_BoundaryViewGeneration)->Unit(benchmark::kMillisecond)->Name("Generate boundary view");
 
 void BM_BoundaryViewGenerationFallback(benchmark::State& state)
 {
@@ -47,7 +47,7 @@ void BM_BoundaryViewGenerationFallback(benchmark::State& state)
         benchmark::DoNotOptimize(boundary_view);
     }
 }
-BENCHMARK(BM_BoundaryViewGenerationFallback)->Unit(benchmark::kSecond);
+BENCHMARK(BM_BoundaryViewGenerationFallback)->Unit(benchmark::kSecond)->Name("Generate boundary view (fallback)");
 
 void BM_MeshOrderConversion(benchmark::State& state)
 {
@@ -61,7 +61,7 @@ void BM_MeshOrderConversion(benchmark::State& state)
         benchmark::DoNotOptimize(converted);
     }
 }
-BENCHMARK(BM_MeshOrderConversion)->Unit(benchmark::kSecond);
+BENCHMARK(BM_MeshOrderConversion)->Unit(benchmark::kSecond)->Name("Convert mesh to 2nd order");
 
 void BM_MeshPartitioning(benchmark::State& state)
 {
@@ -73,4 +73,26 @@ void BM_MeshPartitioning(benchmark::State& state)
         benchmark::DoNotOptimize(parted);
     }
 }
-BENCHMARK(BM_MeshPartitioning)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_MeshPartitioning)->Unit(benchmark::kMillisecond)->Name("Partition mesh in half");
+
+template < typename ExecutionPolicy >
+void BM_ElementIteration(benchmark::State& state)
+{
+    const auto     mesh         = lstr::readMesh(L3STER_TESTDATA_ABSPATH(sphere.msh), lstr::gmsh_tag);
+    const auto&    part         = mesh.getPartitions()[0];
+    constexpr auto read_element = []< lstr::ElementTypes T, lstr::el_o_t O >(const lstr::Element< T, O >& el) {
+        const auto nodes_copy = el.getNodes();
+        benchmark::DoNotOptimize(nodes_copy);
+    };
+
+    for (auto _ : state)
+        part.cvisit(read_element, ExecutionPolicy{});
+}
+BENCHMARK_TEMPLATE(BM_ElementIteration, std::execution::sequenced_policy)
+    ->Unit(benchmark::kMicrosecond)
+    ->UseRealTime()
+    ->Name("Serial element iteration");
+BENCHMARK_TEMPLATE(BM_ElementIteration, std::execution::parallel_policy)
+    ->Unit(benchmark::kMicrosecond)
+    ->UseRealTime()
+    ->Name("Parallel element iteration");
