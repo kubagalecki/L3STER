8eccc183423c0d410e4ba1757c15388c89651b07
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-11-08 15:03:55 +0100

    added basis derivative test

--- tests/LocalAssemblyTests.cpp
+++ tests/LocalAssemblyTests.cpp
@@ -13,7 +13,19 @@ TEST_CASE("Reference bases at QPs test", "[local_asm]")
         constexpr auto   QT   = QuadratureTypes::GLeg;
         constexpr el_o_t QO   = 4;
         const auto&      vals = getRefBasesAtQpoints< QT, QO, ET, EO, BasisTypes::Lagrange >();
-        for (ptrdiff_t i = 0; i < vals.rows(); ++i)
-            CHECK(vals(i, Eigen::all).sum() == Approx{1.}.epsilon(1e-10));
+        for (ptrdiff_t basis = 0; basis < vals.rows(); ++basis)
+            CHECK(vals(basis, Eigen::all).sum() == Approx{1.});
+    }
+
+    SECTION("Derivatives")
+    {
+        constexpr auto   ET   = ElementTypes::Hex;
+        constexpr el_o_t EO   = 4;
+        constexpr auto   QT   = QuadratureTypes::GLeg;
+        constexpr el_o_t QO   = 4;
+        const auto&      ders = getRefBasisDersAtQpoints< QT, QO, ET, EO, BasisTypes::Lagrange >();
+        for (const auto& der : ders)
+            for (ptrdiff_t basis = 0; basis < der.rows(); ++basis)
+                CHECK(der(basis, Eigen::all).sum() == Approx{0.}.margin(1e-14));
     }
 }
