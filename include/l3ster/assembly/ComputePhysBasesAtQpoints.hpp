2f7c14d2469834907d72e33cee5ef7109885038a
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2021-11-09 16:35:26 +0100

    allowed for Jacobian precomputation at qpoints

--- include/l3ster/assembly/ComputePhysBasesAtQpoints.hpp
+++ include/l3ster/assembly/ComputePhysBasesAtQpoints.hpp
@@ -6,22 +6,32 @@
 
 namespace lstr
 {
+namespace detail
+{
+template < ElementTypes ET, el_o_t EO >
+using jacobian_t = Eigen::
+    Matrix< val_t, ElementTraits< Element< ET, EO > >::native_dim, ElementTraits< Element< ET, EO > >::native_dim >;
+
+template < QuadratureTypes QT, q_o_t QO, ElementTypes ET >
+inline constexpr auto quad_size = std::remove_cvref_t< decltype(generateQuadrature< QT, QO, ET >()) >::size;
+} // namespace detail
+
 template < QuadratureTypes QT, q_o_t QO, BasisTypes BT, ElementTypes ET, el_o_t EO >
-auto computePhysicalBasesAtQpoints(const Element< ET, EO >& element)
+auto computePhysicalBasesAtQpoints(
+    const std::array< detail::jacobian_t< ET, EO >, detail::quad_size< QT, QO, ET > >& jacobians)
 {
     const auto& ref_ders = getRefBasisDersAtQpoints< QT, QO, ET, EO, BT >();
     using ret_t          = std::remove_cvref_t< decltype(ref_ders) >;
     ret_t ret_val; // NOLINT initialization at fill-in - note the innermost loop is unrolled by 1
 
-    const auto     jacobian_generator = getNatJacobiMatGenerator(element);
-    constexpr auto el_dim             = ElementTraits< Element< ET, EO > >::native_dim;
-    using jac_t                       = std::invoke_result_t< decltype(jacobian_generator), Point< el_dim > >;
+    constexpr auto el_dim = ElementTraits< Element< ET, EO > >::native_dim;
+    using jac_t           = std::remove_cvref_t< decltype(jacobians) >::value_type;
 
     const auto& quadrature = generateQuadrature< QT, QO, ET >();
 
-    for (ptrdiff_t qp_ind = 0; const auto& qp : quadrature.getPoints())
+    for (size_t qp_ind = 0; qp_ind < quadrature.size; ++qp_ind)
     {
-        const jac_t jacobian_inv = jacobian_generator(Point{qp}).inverse();
+        const jac_t jacobian_inv = jacobians[qp_ind].inverse();
         for (ptrdiff_t der_ind = 0; auto& der : ret_val)
         {
             der(qp_ind, Eigen::all) = ref_ders[0](qp_ind, Eigen::all) * jacobian_inv(der_ind, 0);
@@ -29,10 +39,28 @@ auto computePhysicalBasesAtQpoints(const Element< ET, EO >& element)
                 der(qp_ind, Eigen::all) += ref_ders[i](qp_ind, Eigen::all) * jacobian_inv(der_ind, i);
             ++der_ind;
         }
-        ++qp_ind;
     }
 
     return ret_val;
+}
+
+template < QuadratureTypes QT, q_o_t QO, ElementTypes ET, el_o_t EO >
+auto computeElementJacobiansAtQpoints(const Element< ET, EO >& element)
+{
+    const auto  jac_gen = getNatJacobiMatGenerator(element);
+    const auto& quad    = generateQuadrature< QT, QO, ET >();
+
+    using ret_t = std::array< detail::jacobian_t< ET, EO >, quad.size >;
+    ret_t ret_val; // NOLINT initialization follows immediately below
+    std::ranges::transform(quad.getPoints(), begin(ret_val), [&](const auto& pc) { return jac_gen(Point{pc}); });
+    return ret_val;
+}
+
+template < QuadratureTypes QT, q_o_t QO, BasisTypes BT, ElementTypes ET, el_o_t EO >
+auto computePhysicalBasesAtQpoints(const Element< ET, EO >& element)
+{
+    const auto jac_array = computeElementJacobiansAtQpoints< QT, QO >(element);
+    return computePhysicalBasesAtQpoints< QT, QO, BT, ET, EO >(jac_array);
 }
 } // namespace lstr
 #endif // L3STER_ASSEMBLY_COMPUTEPHYSBASESATQPOINTS_HPP
