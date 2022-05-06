ed4a4476ae7e225d24b1e6925700a1d1c8a2b357
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2022-05-06 14:50:14 +0200

    added test for SP buffering

--- tests/GlobalAssemblyTests.cpp
+++ tests/GlobalAssemblyTests.cpp
@@ -7,6 +7,28 @@
 
 using namespace lstr;
 
+TEST_CASE("Sparsity pattern buffer", "[global_asm]")
+{
+    constexpr auto     n_rows = 1ull << 20, row_size = 1ull << 20;
+    detail::CrsEntries crs_entries{n_rows};
+    auto               test_buf = std::make_unique_for_overwrite< size_t[] >(row_size);
+
+    std::mt19937                            prng{std::random_device{}()};
+    std::uniform_int_distribution< size_t > distribution{0, n_rows - 1};
+
+    const auto test_row  = distribution(prng);
+    auto       write_buf = crs_entries.getRowEntriesForOverwrite(test_row, row_size);
+    std::ranges::generate(write_buf, [&, i = 0]() mutable {
+        const auto val = distribution(prng);
+        test_buf[i++]  = val;
+        return val;
+    });
+
+    const auto read_buf      = crs_entries.getRowEntries(test_row);
+    const auto expected_vals = std::views::counted(test_buf.get(), row_size);
+    CHECK(std::ranges::equal(read_buf, expected_vals));
+}
+
 TEST_CASE("Sparsity pattern assembly", "[global_asm]")
 {
     constexpr std::array node_dist{0., 1., 2., 3.};
