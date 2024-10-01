997dff7d79ba1886887fff2b448a6f1085598c4f
Jakub Gałecki <j.galecki11@gmail.com>
2024-10-01 14:12:03 +0200

    removed -Werror for examples

--- scripts/ci/build-examples.sh
+++ scripts/ci/build-examples.sh
@@ -8,6 +8,6 @@ mkdir -p build
 cd build || exit 1
 cmake \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
-  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_PATH" \
+  -DCMAKE_CXX_COMPILER=mpic++ \
   .. || exit 1
 cmake --build . -- -j "$(grep -c ^processor /proc/cpuinfo)" || exit 1
