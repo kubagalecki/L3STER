5d8164836ca71446670dabb3a1466ded74d19e4f
Jakub Gałecki <j.galecki11@gmail.com>
2025-03-07 08:26:56 +0100

    Domain IDs in primitives can now be specified

--- include/l3ster/mesh/primitives/CubeMesh.hpp
+++ include/l3ster/mesh/primitives/CubeMesh.hpp
@@ -5,10 +5,15 @@
 
 namespace lstr::mesh
 {
+struct CubeMeshIds
+{
+    d_id_t domain = 0, back = 1, front = 2, bottom = 3, top = 4, left = 5, right = 6;
+};
+
 template < std::ranges::random_access_range Rx,
            std::ranges::random_access_range Ry,
            std::ranges::random_access_range Rz >
-auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
+auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz, const CubeMeshIds& ids = {}) -> MeshPartition< 1 >
     requires std::convertible_to< std::ranges::range_value_t< std::decay_t< Rx > >, val_t > and
              std::convertible_to< std::ranges::range_value_t< std::decay_t< Ry > >, val_t > and
              std::convertible_to< std::ranges::range_value_t< std::decay_t< Rz > >, val_t >
@@ -21,13 +26,13 @@ auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
     const size_t e_dz = n_dz - 1;
 
     auto domains = MeshPartition< 1 >::domain_map_t{};
-    domains[0].elements.getVector< Element< ElementType::Hex, 1 > >().reserve(e_dx * e_dy * e_dz);
-    domains[1].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dy);
-    domains[2].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dy);
-    domains[3].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dz);
-    domains[4].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dz);
-    domains[5].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dz * e_dy);
-    domains[6].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dz * e_dy);
+    domains[ids.domain].elements.getVector< Element< ElementType::Hex, 1 > >().reserve(e_dx * e_dy * e_dz);
+    domains[ids.back].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dy);
+    domains[ids.front].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dy);
+    domains[ids.bottom].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dz);
+    domains[ids.top].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dx * e_dz);
+    domains[ids.left].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dz * e_dy);
+    domains[ids.right].elements.getVector< Element< ElementType::Quad, 1 > >().reserve(e_dz * e_dy);
 
     el_id_t el_ind = 0;
 
@@ -54,7 +59,7 @@ auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
                                                            Point{distx[ix + 1], disty[iy], distz[iz + 1]},
                                                            Point{distx[ix], disty[iy + 1], distz[iz + 1]},
                                                            Point{distx[ix + 1], disty[iy + 1], distz[iz + 1]}};
-                emplaceInDomain< ElementType::Hex, 1 >(domains[0], nodes, verts, el_ind++);
+                emplaceInDomain< ElementType::Hex, 1 >(domains[ids.domain], nodes, verts, el_ind++);
             }
         }
     }
@@ -76,8 +81,8 @@ auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
             auto verts2 = verts1;
             std::ranges::for_each(verts2, [&](auto& v) { v = Point{v.x(), v.y(), distz[e_dz]}; });
 
-            emplaceInDomain< ElementType::Quad, 1 >(domains[1], nodes1, verts1, el_ind++);
-            emplaceInDomain< ElementType::Quad, 1 >(domains[2], nodes2, verts2, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.back], nodes1, verts1, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.front], nodes2, verts2, el_ind++);
         }
     }
 
@@ -100,8 +105,8 @@ auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
             auto verts2 = verts1;
             std::ranges::for_each(verts2, [&](auto& v) { v = Point{v.x(), disty[e_dy], v.z()}; });
 
-            emplaceInDomain< ElementType::Quad, 1 >(domains[3], nodes1, verts1, el_ind++);
-            emplaceInDomain< ElementType::Quad, 1 >(domains[4], nodes2, verts2, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.bottom], nodes1, verts1, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.top], nodes2, verts2, el_ind++);
         }
     }
 
@@ -124,19 +129,19 @@ auto makeCubeMesh(Rx&& distx, Ry&& disty, Rz&& distz) -> MeshPartition< 1 >
             auto verts2 = verts1;
             std::ranges::for_each(verts2, [&](auto& v) { v = Point{distx[e_dx], v.y(), v.z()}; });
 
-            emplaceInDomain< ElementType::Quad, 1 >(domains[5], nodes1, verts1, el_ind++);
-            emplaceInDomain< ElementType::Quad, 1 >(domains[6], nodes2, verts2, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.left], nodes1, verts1, el_ind++);
+            emplaceInDomain< ElementType::Quad, 1 >(domains[ids.right], nodes2, verts2, el_ind++);
         }
     }
 
-    return {std::move(domains), 0, n_dx * n_dy * n_dz, {1, 2, 3, 4, 5, 6}};
+    return {std::move(domains), 0, n_dx * n_dy * n_dz, {ids.back, ids.front, ids.bottom, ids.top, ids.left, ids.right}};
 }
 
 template < std::ranges::random_access_range R >
-auto makeCubeMesh(R&& dist) -> MeshPartition< 1 >
+auto makeCubeMesh(R&& dist, const CubeMeshIds& ids = {}) -> MeshPartition< 1 >
     requires std::convertible_to< std::ranges::range_value_t< std::decay_t< R > >, val_t >
 {
-    return makeCubeMesh(dist, dist, dist);
+    return makeCubeMesh(dist, dist, dist, ids);
 }
 } // namespace lstr::mesh
 #endif // L3STER_CUBEMESH_HPP
