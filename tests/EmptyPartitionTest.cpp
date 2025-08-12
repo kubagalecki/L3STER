552761046ee515e94e0fca4b2c517eadf7834d09
Jakub Gałecki <j.galecki11@gmail.com>
2025-08-12 10:09:20 +0200

    Empty partition test now also checks matrix-free system

--- tests/EmptyPartitionTest.cpp
+++ tests/EmptyPartitionTest.cpp
@@ -8,7 +8,7 @@
 
 using namespace lstr;
 
-template < CondensationPolicy CP >
+template < OperatorEvaluationStrategy S, CondensationPolicy CP = {} >
 void test()
 {
     constexpr d_id_t domain_id   = 0;
@@ -19,7 +19,7 @@ void test()
     const auto     mesh       = generateAndDistributeMesh< mesh_order >(
         *comm, [&] { return mesh::makeSquareMesh(std::array{0., 1.}); }, {}, problem_def);
 
-    constexpr auto alg_params       = AlgebraicSystemParams{.cond_policy = CP};
+    constexpr auto alg_params       = AlgebraicSystemParams{.eval_strategy = S, .cond_policy = CP};
     constexpr auto algparams_ctwrpr = util::ConstexprValue< alg_params >{};
     auto           alg_sys          = makeAlgebraicSystem(comm, mesh, problem_def, {}, algparams_ctwrpr);
     alg_sys.describe();
@@ -36,6 +36,7 @@ int main(int argc, char* argv[])
 {
     const auto max_par_guard = util::MaxParallelismGuard{4};
     const auto scope_guard   = L3sterScopeGuard{argc, argv};
-    test< CondensationPolicy::None >();
-    test< CondensationPolicy::ElementBoundary >();
+    test< OperatorEvaluationStrategy::GlobalAssembly, CondensationPolicy::None >();
+    test< OperatorEvaluationStrategy::GlobalAssembly, CondensationPolicy::ElementBoundary >();
+    test< OperatorEvaluationStrategy::MatrixFree >();
 }
