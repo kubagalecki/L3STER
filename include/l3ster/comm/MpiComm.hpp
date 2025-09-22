f68f41b1f64fbd20b60fef22b54ce50679b53310
Jakub Gałecki <j.galecki11@gmail.com>
2025-09-22 10:47:49 +0200

    MPI exceptions now report error codes

--- include/l3ster/comm/MpiComm.hpp
+++ include/l3ster/comm/MpiComm.hpp
@@ -48,12 +48,13 @@ L3STER_MPI_TYPE_MAPPING_STRUCT(long double, MPI_LONG_DOUBLE)               // NO
 L3STER_MPI_TYPE_MAPPING_STRUCT(std::byte, MPI_BYTE)                        // NOLINT
 
 inline void
-handleMPIError(int error, std::string_view err_msg, std::source_location src_loc = std::source_location::current())
+handleMPIError(int error, std::string_view fun_name, std::source_location src_loc = std::source_location::current())
 {
+    const auto err_msg = std::format("MPI error: call to {} failed with error code {}", fun_name, error);
     util::throwingAssert(not error, err_msg, src_loc);
 }
 
-#define L3STER_INVOKE_MPI(fun__, ...) comm::handleMPIError(fun__(__VA_ARGS__), "Call to " #fun__ " failed")
+#define L3STER_INVOKE_MPI(fun__, ...) comm::handleMPIError(fun__(__VA_ARGS__), #fun__)
 
 template < typename T >
 concept MpiBuiltinType_c = requires { MpiType< std::remove_cvref_t< T > >::value(); };
