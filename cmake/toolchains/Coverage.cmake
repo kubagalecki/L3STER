6fa41f8a30250cda93da956e4e56a2c70a8f411d
Jakub Gałecki <j.galecki11@gmail.com>
2026-01-11 10:07:36 +0100

    disable interference size warning in CI

--- cmake/toolchains/Coverage.cmake
+++ cmake/toolchains/Coverage.cmake
@@ -1,3 +1,3 @@
 # Toolchain for code coverage using gcc
 set( CMAKE_CXX_COMPILER mpicxx )
-set( CMAKE_CXX_FLAGS_INIT "-march=native -mtune=native -fprofile-arcs -fprofile-abs-path -ftest-coverage -fPIC -lgcov --coverage -p" )
+set( CMAKE_CXX_FLAGS_INIT "-march=native -mtune=native -Wno-interference-size -fprofile-arcs -fprofile-abs-path -ftest-coverage -fPIC -lgcov --coverage -p" )
