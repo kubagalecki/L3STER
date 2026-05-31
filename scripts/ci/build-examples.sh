5018ee74c4ea28baffd6d3c448037a25f38bc485
Jakub Gałecki <j.galecki11@gmail.com>
2026-05-31 10:11:57 +0200

    update CI

--- scripts/ci/build-examples.sh
+++ scripts/ci/build-examples.sh
@@ -10,4 +10,4 @@ cmake \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DCMAKE_CXX_COMPILER=mpic++ \
   .. || exit 1
-cmake --build . -- -j "$(grep -c ^processor /proc/cpuinfo)" || exit 1
+cmake --build . -- -j "$(nproc)" || exit 1
